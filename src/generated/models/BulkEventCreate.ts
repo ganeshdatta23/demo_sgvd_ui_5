81f4cd274ea14e2465857577f7c256d70b83114f
Ganesh Datta <ganeshdattapadamata@gmail.com>
2026-01-15 22:59:11 +0530

    build: ignore generated source directories in .gitignore.

--- src/generated/models/BulkEventCreate.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { EventCreate } from './EventCreate';
-export type BulkEventCreate = {
-    events: Array<EventCreate>;
-};
-
